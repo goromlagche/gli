3d21e24d685474064b4b390f29ff7df2c75ea118
goromlagche <mrinmoy.das91@gmail.com>
2016-12-16 21:55:07 +0530

    write custom show instance for types

--- src/gli/gitlab.hs
+++ src/gli/gitlab.hs
@@ -38,44 +38,25 @@ account x = do
 printProject :: (String -> AccountConfg) -> Project -> IO ()
 printProject configs project = do
   putStrLn "\\------------------------------------------------------------------------/"
-  putStrLn $ T.unpack $ projectData project
+  print project
   pRs <- mergeRequests configs $ id (project :: Project)
-  putStrLn $ T.unpack pRs
+  putStrLn pRs
   putStrLn "/------------------------------------------------------------------------\\"
 
 parseProject :: BLI.ByteString -> Maybe [Project]
 parseProject body = decode body :: Maybe [Project]
 
-projectData :: Project -> T.Text
-projectData project =
-  T.unlines [ ""
-            , T.concat ["Name:       ", name (project :: Project)]
-            , T.concat ["Descripion: ", justOrEmpty $ description (project :: Project)]
-            , ""]
-
-mergeRequests :: (String -> AccountConfg) -> Int -> IO T.Text
+mergeRequests :: (String -> AccountConfg) -> Int -> IO String
 mergeRequests configs pid = do
   prResponseBody <- apiCall $
                     configs ( "/projects/" ++
                               show pid ++
                               "/merge_requests?state=opened")
   let body = justBody $ parseMergeRequest prResponseBody
-  return (T.unlines (map printMergeRequest body))
+  return (unlines (map show body))
 
 parseMergeRequest :: BLI.ByteString -> Maybe [MergeRequest]
 parseMergeRequest body = decode body :: Maybe [MergeRequest]
-
-printMergeRequest :: MergeRequest -> T.Text
-printMergeRequest pr =
-  T.unlines [ T.concat ["URL:        ", web_url pr]
-            , T.concat ["Title:      ", justOrEmpty $ title pr]
-            , T.concat ["Descripion: ", justOrEmpty $ description (pr :: MergeRequest)]
-            , T.concat ["Author:     ", (username . author) pr]
-            , ""]
-
-justOrEmpty :: Maybe T.Text -> T.Text
-justOrEmpty (Just a)  = a
-justOrEmpty Nothing = ""
 
 justBody :: Maybe [a] -> [a]
 justBody Nothing = []
--- src/gli/types.hs
+++ src/gli/types.hs
@@ -12,36 +12,64 @@ data Project =
   Project { id          :: Int
           , description :: Maybe T.Text
           , name        :: T.Text
-          } deriving (Generic, Show)
+          } deriving (Generic)
 
-data MergeRequest = MergeRequest { id                          :: Int
-                                 , title                       :: Maybe T.Text
-                                 , iid                         :: Int
-                                 , project_id                  :: Int
-                                 , description                 :: Maybe T.Text
-                                 , upvotes                     :: Int
-                                 , downvotes                   :: Int
-                                 , author                      :: User
-                                 , assignee                    :: Maybe User
-                                 , source_project_id           :: Int
-                                 , target_project_id           :: Int
-                                 , labels                      :: [Maybe T.Text]
-                                 , work_in_progress            :: Bool
-                                 , milestone                   :: Maybe T.Text
-                                 , merge_when_build_succeeds   :: Bool
-                                 , merge_status                :: T.Text
-                                 , sha                         :: T.Text
-                                 , merge_commit_sha            :: Maybe T.Text
-                                 , subscribed                  :: Maybe Bool
-                                 , user_notes_count            :: Int
-                                 , should_remove_source_branch :: Maybe Bool
-                                 , force_remove_source_branch  :: Maybe Bool
-                                 , web_url                     :: T.Text
-                                 } deriving (Generic, Show)
+instance Show Project where
+   show (Project pid pdescription pname) =
+       unlines [ ""
+               , "Id:         " ++ show pid
+               , "Name:       " ++ show pname
+               , "Descripion: " ++ justOrEmpty pdescription
+               , ""
+               ]
+
+data MergeRequest = MergeRequest { id                        :: Int
+                                 , title                     :: Maybe T.Text
+                                 , iid                       :: Int
+                                 , project_id                :: Int
+                                 , description               :: Maybe T.Text
+                                 , upvotes                   :: Int
+                                 , downvotes                 :: Int
+                                 , author                    :: User
+                                 , assignee                  :: Maybe User
+                                 , source_project_id         :: Int
+                                 , target_project_id         :: Int
+                                 , labels                    :: [Maybe T.Text]
+                                 , work_in_progress          :: Bool
+                                 , milestone                 :: Maybe T.Text
+                                 , merge_when_build_succeeds :: Bool
+                                 , merge_status              :: T.Text
+                                 , subscribed                :: Maybe Bool
+                                 , web_url                   :: T.Text
+                                 , sha                       :: T.Text
+                                 , created_at                :: T.Text
+                                 , updated_at                :: T.Text
+                                 } deriving (Generic)
+
+instance Show MergeRequest where
+   show (MergeRequest mid mtitle _ _ mdescription _
+         _ mauthor massignee _
+         _ _ mwork_in_progress _
+         _ mmerge_status _ mweb_url _ mcreated mupdated) =
+     unlines [ "ID:         " ++  show mid
+             , "URL:        " ++  show mweb_url
+             , "Title:      " ++  justOrEmpty mtitle
+             , "Descripion: " ++  justOrEmpty mdescription
+             , "Author:     " ++  show mauthor
+             , "Assignee:   " ++  justOrEmpty massignee
+             , "WIP:        " ++  show mwork_in_progress
+             , "Status:     " ++  show mmerge_status
+             , "Created At: " ++  show mcreated
+             , "Updated At: " ++  show mupdated
+             , ""
+             ]
 
 data User = User { name     :: T.Text
                  , username :: T.Text
-                 } deriving (Generic, Show)
+                 } deriving (Generic)
+
+instance Show User where
+   show (User _ uusername) = show uusername
 
 data GliCfg = GliCfg { accounts :: [Account]
                      } deriving (Generic, Show)
@@ -65,3 +93,7 @@ instance FromJSON User
 instance FromJSON GliCfg
 instance FromJSON Account
 instance FromJSON Project
+
+justOrEmpty :: Show a => Maybe a -> String
+justOrEmpty (Just a)  = show a
+justOrEmpty Nothing = ""
